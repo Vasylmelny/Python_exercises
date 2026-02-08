d8a0bf108e49617e17308a11a0cf29aff64382ec
Vasylmelny <vasylmelny@gmail.com>
2026-02-08 14:56:25 +0200

    anything old ex

--- Exercise from Pynative/ex_dict_7.py
+++ Exercise from Pynative/ex_dict_7.py
@@ -1,20 +1,5 @@
-# Create Higher-Order Function
-# Write a function apply_operation(func, x, y)
-# that takes a function func and two numbers x and y as arguments,
-# and returns the result of calling func(x, y).
-# Demonstrate its use with different functions (e.g., addition, subtraction).
-#
-# The exercise requires you to create a higher-order function,
-# which is a function that can take other functions as arguments.
+# Given a nested dictionary {'person': {'name': 'Alice', 'age': 30}},
+# print Alice’s age.
 
-def mul(x, y):
-    return x * y
-
-def div(x, y):
-    return x / y
-
-def apply_operation(func, x, y):
-    return func(x, y)
-
-print(apply_operation(mul, 2, 3))
-print(apply_operation(div, 9, 3))
+nesdic = {'person': {'name': 'Alice', 'age': 30}}
+print("Alice's age is:", nesdic['person']['age'])
